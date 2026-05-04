2c9de681f15d0a17dc2291aa888ab5d3cef92b30
Antonio <antojoseabad@gmail.com>
2026-05-04 22:50:56 +0200

    feat: add cache for product requests

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -18,19 +18,41 @@ interface Options {
 export class ProductsService {
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
+
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
 
-    return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
-      params: {
-        limit: limit,
-        offset: offset,
-        gender: gender,
-      },
-    });
+    const key = `${limit}-${offset}-${gender}`;
+    if (key && this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
+
+    return this.http
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit: limit,
+          offset: offset,
+          gender: gender,
+        },
+      })
+      .pipe(
+        tap((response) => {
+          this.productsCache.set(key, response);
+        }),
+      );
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if (idSlug && this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      tap((product) => {
+        this.productCache.set(idSlug, product);
+      }),
+    );
   }
 }
